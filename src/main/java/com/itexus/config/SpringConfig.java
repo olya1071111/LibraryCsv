eafe569ba8a9e3da690e7dca3210edc54bc28fa1
Babich Olga <homka-ru@yandex.ru>
2024-09-09 10:37:26 +0300

    add controller layer

---
+++ src/main/java/com/itexus/config/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.itexus.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.itexus")
+public class SpringConfig {
+
+}
